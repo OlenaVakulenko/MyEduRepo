d12d5feda1983dd8f449e78277bc6c836503f5c9
Olenka <send.olena@gmail.com>
2016-12-03 22:07:23 +0200

    Corrected bug in sorting method

--- OOP/src/main/Main.java
+++ OOP/src/main/Main.java
@@ -12,7 +12,6 @@ public class Main {
         Random x = new Random();
         Random side = new Random();
         figur.Figur[] Figures = new figur.Figur[10];
-        double Areas[] = new double[10];
         for (int i = 1; i < 11; i++) {
             int random = r.nextInt(3);
             double sideNew = side.nextInt(100);
@@ -31,35 +30,33 @@ public class Main {
 
             Random k = new Random();
             someFigure.resize(k.nextDouble());
-            double area = someFigure.area();
             Figures[i - 1] = someFigure;
-            Areas[i - 1] = area;
-        }
+            }
         // Фигуры и площади до сортировки
         System.out.println("Фигуры и площади до сорторовки");
         for (int step = 0; step < Figures.length - 1; step++) {
             System.out.println(Figures[step]);
-            System.out.println(Areas[step]);
+            System.out.println(Figures[step].area());
         }
-        bubbleSort(Areas);
+        bubbleSort(Figures);
         // Фигуры и площади после сортировки
         System.out.println("Фигуры и площади после сорторовки");
         for (int step = 0; step < Figures.length - 1; step++) {
             System.out.println(Figures[step]);
-            System.out.println(Areas[step]);
+            System.out.println(Figures[step].area());
         }
 
     }
 
 
-    public static void bubbleSort(double[] arr) {
+    public static void bubbleSort(figur.Figur[] arr) {
     /*Внешний цикл каждый раз сокращает фрагмент массива,
       так как внутренний цикл каждый раз ставит в конец
       фрагмента максимальный элемент*/
         for (int i = arr.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    double tmp = arr[j];
+                if (arr[j].area() > arr[j + 1].area()) {
+                    figur.Figur tmp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = tmp;
                 }
